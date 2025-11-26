313f6641f180657d29091d91fd7d2c64fd41f5bb
Artem <artem90078@gmail.com>
2025-11-26 21:02:10 +0200

    complite

--- app/main.py
+++ app/main.py
@@ -1,8 +1,53 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class, clean_mark, brand):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = round(average_rating, 1)
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car):
+        """
+        Calculates cost: (comfort_class * diff_clean * rating) / distance
+        """
+        diff = self.clean_power - car.clean_mark
+        cost = (car.comfort_class * diff * self.average_rating) / self.distance_from_city_center
+        return round(cost, 1)
+
+    def wash_single_car(self, car):
+        """
+        Washes a car to the station's clean_power level if the car is dirtier.
+        """
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def serve_cars(self, cars):
+        """
+        Processes a list of cars. Only washes cars where car.clean_mark < station.clean_power.
+        Returns total income rounded to 1 decimal.
+        """
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                # Calculate price BEFORE washing to get the correct difference
+                price = self.calculate_washing_price(car)
+                income += price
+                # Perform the wash
+                self.wash_single_car(car)
+
+        return round(income, 1)
+
+    def rate_service(self, new_rate):
+        """
+        Updates average rating and count of ratings based on a new input rate.
+        """
+        total_rating_score = self.average_rating * self.count_of_ratings
+        total_rating_score += new_rate
+        self.count_of_ratings += 1
+        self.average_rating = round(total_rating_score / self.count_of_ratings, 1)
